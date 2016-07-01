cef9071e54bf52f8583ad80d2fc5aedf6a05a01c
Benedikt Bock <Benedikt.Bock@dm.de>
2016-07-01 15:57:50 +0200

    Bugfix with using --warningx and --criticalx in checkpoint mem subsystem

--- plugins-scripts/Classes/CheckPoint/Firewall1/Component/MemSubsystem.pm
+++ plugins-scripts/Classes/CheckPoint/Firewall1/Component/MemSubsystem.pm
@@ -16,16 +16,16 @@ sub init {
 sub check {
   my $self = shift;
   $self->add_info(sprintf 'memory usage is %.2f%%', $self->{memory_usage});
-  $self->set_thresholds(warning => 80, critical => 90);
-  $self->add_message($self->check_thresholds($self->{memory_usage}));
+  $self->set_thresholds(metric => 'memory_usage', warning => 80, critical => 90);
+  $self->add_message($self->check_thresholds(metric => 'memory_usage', value => $self->{memory_usage}));
   $self->add_perfdata(
       label => 'memory_usage',
       value => $self->{memory_usage},
       uom => '%',
   );
   $self->add_info(sprintf 'swap usage is %.2f%%', $self->{swap_usage});
-  $self->set_thresholds(warning => 30, critical => 75);
-  $self->add_message($self->check_thresholds($self->{swap_usage}));
+  $self->set_thresholds(metric => 'swap_usage', warning => 30, critical => 75);
+  $self->add_message($self->check_thresholds(metric => 'swap_usage', value => $self->{swap_usage}));
   $self->add_perfdata(
       label => 'swap_usage',
       value => $self->{swap_usage},
